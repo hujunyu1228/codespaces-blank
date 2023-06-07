fb14ef94ae5304d78f3b7418bd18b2707fcefa95
hujunyu1228 <117439519+hujunyu1228@users.noreply.github.com>
2023-06-07 13:35:24 +0000

    Changes to be committed: 	modified:   fuckyoutulun/binchaji/1536.cpp

--- fuckyoutulun/binchaji/1536.cpp
+++ fuckyoutulun/binchaji/1536.cpp
@@ -12,7 +12,18 @@ int f(int x){
     if(x==fa[x])return x;
     return fa[x]=f(fa[x]);
 }
-// int m(int x,int y){}
+int m(int a,int b){
+    int x=f(a);
+    int y=f(b);
+    if(x!=y){
+        fa[x]=y;
+    }
+}
 int main(){
-    
+    int n,m;
+    while(cin>>n>>m){
+        int a,b;
+        cin>>a>>b;
+        m(a,b);
+    }
 }
